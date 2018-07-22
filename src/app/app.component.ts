685876d30ada4d071cccfeffffc204ad111bc662
Sanjeev Kumar <kh2264@kitspl.com>
2018-07-22 16:58:47 +0530

    Registration page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl,FormGroup} from '@angular/forms';
+import {FormControl,FormGroup,FormBuilder,Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,4 +9,14 @@ export class AppComponent {
   angularForm = new FormGroup({
     name : new FormControl()
   })
+
+constructor(private fb : FormBuilder){
+  this.createForm();
+}
+createForm() {
+  this.angularForm = this.fb.group({
+     email: ['',Validators.required],
+     password : ['',Validators.required],
+  });
+}
 }
